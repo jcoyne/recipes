762c6d08fb2e9cf8cc1df1435bb24fdee2c93e5f
Justin Coyne <jcoyne@justincoyne.com>
2015-12-08 10:14:57 -0600

    Route to the correct sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
+
   def create
     headers['Access-Control-Allow-Origin'] = '*'
     respond_to do |format|
