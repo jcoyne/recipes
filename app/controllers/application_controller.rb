13dffd6f44bf0ba88341ffceadcb4168320d7b27
Justin Coyne <jcoyne@justincoyne.com>
2015-12-28 22:01:46 -0600

    Switch to using the ember app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  acts_as_token_authentication_handler_for User
+
+  # TODO if we turn this on it's going to check the token. Currently our front-end isn't sending
+  # the token 
+  # acts_as_token_authentication_handler_for User
 end
