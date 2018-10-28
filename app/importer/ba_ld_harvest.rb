d5a139c4f455339cd5e6a8eacb6610ff9b612f1c
Justin Coyne <jcoyne@justincoyne.com>
2018-10-27 21:05:51 -0500

    Update LD harvester

--- app/importer/ba_ld_harvest.rb
+++ app/importer/ba_ld_harvest.rb
@@ -3,11 +3,13 @@ class BaLdHarvest < Harvest
   def self.parse(html, uri_str)
     ld_node = html.css('script[type="application/ld+json"]')
     json = JSON.parse(ld_node.text)
-    { title: json['name'],
+    {
+      title: json['name'],
       link: uri_str,
       thumbnail: json['image'],
       servings: json['recipeYield'],
       ingredients: json['recipeIngredient'],
-      preparation_steps: json['recipeInstructions'] }.tap { |x| puts x }
+      preparation_steps: json['recipeInstructions'].map { |how_to_step| how_to_step['text'] }
+    }.tap { |x| puts x }
   end
 end
