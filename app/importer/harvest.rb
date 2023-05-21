6b64289d8047228e0e9799e79ffbce731aefee17
Justin Coyne <jcoyne@justincoyne.com>
2023-05-21 13:51:28 -0500

    Use URI.open

--- app/importer/harvest.rb
+++ app/importer/harvest.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class Harvest
   def self.harvest(uri_str)
     doc = begin
-            open(uri_str)
+            URI.open(uri_str)
           rescue Errno::ENOENT
             $stderr.puts "Unable to open #{uri_str}"
             exit
